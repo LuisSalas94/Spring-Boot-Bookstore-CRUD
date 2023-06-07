8bb7b230258704f63972e9b78ff3d31d788436c4
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-07 09:44:12 -0500

    Create Book Entity

---
+++ src/main/java/com/bookstore/springboot/entity/Book.java
@@ -0,0 +1,30 @@
+package com.bookstore.springboot.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String description;
+
+    @Column(nullable = false)
+    private String genre;
+}
